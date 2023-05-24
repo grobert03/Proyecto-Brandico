9bf22aeb865efc5f76f8590388f208495bbc5108
grobert03 <gainarobert47@gmail.com>
2023-05-24 08:27:22 +0200

    admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -12,7 +12,9 @@ const devolverUsuarios = (nombre, rol, usuarios, empresas) => {
     async: true,
     success: function (data) {
       console.log(data);
-      data = data.filter((usu) => usu.nombre.toLowerCase().startsWith(nombre));
+      data = data.filter((usu) =>
+        usu.nombre.toLowerCase().startsWith(nombre.toLowerCase())
+      );
       if (data.length == 0) {
         $("tbody").empty();
         $("tbody").append(
@@ -24,16 +26,183 @@ const devolverUsuarios = (nombre, rol, usuarios, empresas) => {
         $("tbody").empty();
         data.forEach((e) => {
           $("tbody").append(
-            $(`<tr><td>${e.correo}</td>
+            $(`<tr data-correo="${e.correo}"><td>${e.correo}</td>
             <td>${e.nombre}</td>
             <td>${e.apellidos ? e.apellidos : ""}</td>
             <td>${e.rol ? e.rol : ""}</td>
             <td>${e.dni ? e.dni : e.cif}</td>
             <td>${e.telefono}</td>
             <td>${e.direccion ? e.direccion : ""}</td>
-            <td>${e.provincia ? e.provincia : ""}</td></tr>`)
+            <td>${e.provincia ? e.provincia : ""}</td>
+            <td><button class='button data-correo="${
+              e.correo
+            }" modificar is-warning'><i class="fa-solid fa-pencil" style="color: #004080;"></i></button><button data-correo="${
+              e.correo
+            }" class='borrar button is-danger'><i class="fa-solid fa-x" style="color: #ffffff;"></i></button></td>
+            </tr>`)
           );
         });
+
+        function convertRowToInputs(row) {
+          row.find("td").each(function () {
+            if ($(this).children("button").length == 0) {
+              var text = $(this).text().trim();
+              var input = $('<input class="input" type="text">').val(text);
+              $(this).html(input);
+            }
+          });
+          var btnMod= row.find(".modificar");
+          var btnBorrar = row.find(".borrar");
+          var confirmBtn = $(
+            '<button class="confirm button is-success"><i class="fa-solid fa-check" style="color: #ffffff;"></i></button>'
+          );
+          var cancelBtn = $(
+            '<button class="cancel button is-danger"><i class="fa-solid fa-times" style="color: #ffffff;"></i></button>'
+          );
+          btnMod.replaceWith(confirmBtn);
+          btnBorrar.replaceWith(cancelBtn);
+
+          $(".confirm").unbind().click(function () {
+            let aBuscar = $(this).parent().parent().data("correo");
+            if ($(this).parent().parent().children("td").eq(3).children("input").eq(0).val() == '') {
+              let correo = $(this).parent().parent().children("td").eq(0).children("input").eq(0).val();
+              let nombrex = $(this).parent().parent().children("td").eq(1).children("input").eq(0).val();
+              let cif = $(this).parent().parent().children("td").eq(4).children("input").eq(0).val();
+              let telefono = $(this).parent().parent().children("td").eq(5).children("input").eq(0).val();
+              let direccion = $(this).parent().parent().children("td").eq(6).children("input").eq(0).val();
+              let provincia = $(this).parent().parent().children("td").eq(7).children("input").eq(0).val();
+              $.ajax({
+                url: ruta_modificar_empresa,
+                method: "POST",
+                dataType: "json",
+                data: {
+                  aBuscar: aBuscar,
+                  correo: correo,
+                  nombre: nombrex,
+                  cif: cif,
+                  telefono: telefono,
+                  direccion: direccion,
+                  provincia: provincia
+                },
+                async: true,
+                success: function (data) {
+                  alert("Empresa modificada!");
+                  devolverUsuarios(nombre, rol, usuarios, empresas);
+                },
+                error: function (errorThrown) {
+                  console.log(errorThrown);
+                },
+              });
+            } else {
+              let aBuscar = $(this).parent().parent().data("correo");
+              let correo = $(this).parent().parent().children("td").eq(0).children("input").eq(0).val();
+              let nombrex = $(this).parent().parent().children("td").eq(1).children("input").eq(0).val();
+              let apellidos = $(this).parent().parent().children("td").eq(2).children("input").eq(0).val();
+              let rolx = $(this).parent().parent().children("td").eq(3).children("input").eq(0).val();
+              let dni = $(this).parent().parent().children("td").eq(4).children("input").eq(0).val();
+              let telefono = $(this).parent().parent().children("td").eq(5).children("input").eq(0).val();
+
+              $.ajax({
+                url: ruta_modificar_usuario,
+                method: "POST",
+                dataType: "json",
+                data: {
+                  aBuscar: aBuscar,
+                  correo: correo,
+                  nombre: nombrex,
+                  apellidos: apellidos,
+                  rol: rolx,
+                  dni: dni,
+                  telefono: telefono
+                },
+                async: true,
+                success: function (data) {
+                  alert("Usuario modificado!");
+                  devolverUsuarios(nombre, rol, usuarios, empresas);
+                },
+                error: function (errorThrown) {
+                  console.log(errorThrown);
+                },
+              });
+            }
+          })
+        }
+
+        function convertRowToNormal(row) {
+          row.find("td input").each(function () {
+            var text = $(this).val();
+            $(this).replaceWith(text);
+          });
+          var confirmBtn = row.find(".confirm");
+          var cancelBtn = row.find(".cancel");
+          var modificarBtn = $(
+            '<button class="modificar button is-warning"><i class="fa-solid fa-pencil" style="color: #004080;"></i></button>'
+          );
+          var borrarBtn = $(
+            '<button class="borrar button is-danger"><i class="fa-solid fa-x" style="color: #ffffff;"></i></button>'
+          );
+          confirmBtn.replaceWith(modificarBtn);
+          cancelBtn.replaceWith(borrarBtn);
+
+          $(".borrar")
+          .unbind()
+          .click(function () {
+            let c = confirm("Estás seguro de que quieres borrar el usuario?");
+            if (c) {
+              $.ajax({
+                url: ruta_borrar_usuario,
+                method: "POST",
+                dataType: "json",
+                data: {
+                  correo: $(this).data("correo"),
+                },
+                async: true,
+                success: function (data) {
+                  alert("Usuario borrado!");
+                  devolverUsuarios(nombre, rol, usuarios, empresas);
+                },
+                error: function (errorThrown) {
+                  console.log(errorThrown);
+                },
+              });
+            }
+          });
+        }
+
+        $(document).on("click", ".modificar", function () {
+          var row = $(this).closest("tr");
+          convertRowToInputs(row);
+        });
+
+        $(document).on("click", ".cancel", function () {
+          var row = $(this).closest("tr");
+          convertRowToNormal(row);
+          devolverUsuarios(nombre, rol, usuarios, empresas);
+        });
+
+        $(".borrar")
+          .unbind()
+          .click(function () {
+            let c = confirm("Estás seguro de que quieres borrar el usuario?");
+            if (c) {
+              $.ajax({
+                url: ruta_borrar_usuario,
+                method: "POST",
+                dataType: "json",
+                data: {
+                  correo: $(this).data("correo"),
+                },
+                async: true,
+                success: function (data) {
+                  alert("Usuario borrado!");
+                  devolverUsuarios(nombre, rol, usuarios, empresas);
+                },
+                error: function (errorThrown) {
+                  console.log(errorThrown);
+                },
+              });
+            }
+          });
       }
     },
     error: function (errorThrown) {
@@ -41,6 +210,58 @@ const devolverUsuarios = (nombre, rol, usuarios, empresas) => {
     },
   });
 };
+
+$("#btn-crear-usu").click(() => {
+  // Empresa
+  if ($("#input-dir").val().length > 0) {
+    $.ajax({
+      url: ruta_crear_empresa,
+      method: "POST",
+      data: {
+        correo: $("#input-correo").val(),
+        pass: $("#input-pass").val(),
+        nombre: $("#input-nombre").val(),
+        tel: $("#input-tel").val(),
+        cif: $("#input-cif").val(),
+        dir: $("#input-dir").val(),
+        prov: $("#input-prov").val(),
+      },
+      async: true,
+      dataType: "json",
+      success: function (data) {
+        alert("Empresa creada!");
+        devolverUsuarios(nombre, rol, usuarios, empresas);
+      },
+      error: function (errorThrown) {
+        console.log(errorThrown);
+      },
+    });
+  } else {
+    // Usuario
+    $.ajax({
+      url: ruta_crear_usuario,
+      method: "POST",
+      data: {
+        correo: $("#input-correo").val(),
+        pass: $("#input-pass").val(),
+        nombre: $("#input-nombre").val(),
+        tel: $("#input-tel").val(),
+        dni: $("#input-dni").val(),
+        ape: $("#input-ape").val(),
+        rol: $("#select-rol").val(),
+      },
+      async: true,
+      dataType: "json",
+      success: function (data) {
+        alert("Usuario creado!");
+        devolverUsuarios(nombre, rol, usuarios, empresas);
+      },
+      error: function (errorThrown) {
+        console.log(errorThrown);
+      },
+    });
+  }
+});
 
 devolverUsuarios(nombre, rol, usuarios, empresas);
 
--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -64,4 +64,138 @@ class AdminController extends AbstractController
 
         return new JsonResponse($json);
     }
+
+    /**
+     * @Route("/borrar", name="borrar_usuario")
+     */
+    public function borrarUsuario(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $correo = $request->request->get('correo');
+
+        $usu = $em->getRepository(Usuario::class)->findOneBy(['correo' => $correo]);
+
+        if ($usu) {
+            $em->remove($usu);
+        } else {
+            $emp = $em->getRepository(Empresa::class)->findOneBy(['correo' => $correo]);
+            $em->remove($emp);
+        }
+        $em->flush();
+        return new JsonResponse(['borrado' => true]);
+    }
+
+    /**
+     * @Route("/crearUsu", name="crear_usuario")
+     */
+    public function crearUsu(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $correo = $request->request->get('correo');
+        $pass = $request->request->get('pass');
+        $nombre = $request->request->get('nombre');
+        $tel = $request->request->get('tel');
+        $dni = $request->request->get('dni');
+        $ape = $request->request->get('ape');
+        $rol = $request->request->get('rol');
+
+        $usu = new Usuario();
+        $usu->setCorreo($correo);
+        $usu->setClave(password_hash($pass, PASSWORD_DEFAULT));
+        $usu->setDni($dni);
+        $usu->setNombre($nombre);
+        $usu->setApellidos($ape);
+        $usu->setTelefono($tel);
+        $usu->setRol($rol);
+
+        $em->persist($usu);
+        $em->flush();
+
+        return new JsonResponse(['creado' => true]);
+    }
+
+    /**
+     * @Route("/crearEmpresa", name="crear_empresa")
+     */
+    public function crearEmpresa(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $correo = $request->request->get('correo');
+        $pass = $request->request->get('pass');
+        $nombre = $request->request->get('nombre');
+        $tel = $request->request->get('tel');
+        $cif = $request->request->get('cif');
+        $dir = $request->request->get('dir');
+        $prov = $request->request->get('prov');
+
+        $empresa = new Empresa();
+        $empresa->setCorreo($correo);
+        $empresa->setClave(password_hash($pass, PASSWORD_DEFAULT));
+        $empresa->setCif($cif);
+        $empresa->setNombre($nombre);
+        $empresa->setTelefono($tel);
+        $empresa->setDireccion($dir);
+        $empresa->setProvincia($prov);
+
+        $em->persist($empresa);
+        $em->flush();
+
+        return new JsonResponse(['creado' => true]);
+    }
+
+    /**
+     * @Route("/modificarEmpresa", name="modificar_empresa")
+     */
+    public function modificarEmpresa(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $buscar = $request->request->get('aBuscar');
+        $correo = $request->request->get('correo');
+        $nombre = $request->request->get('nombre');
+        $tel = $request->request->get('telefono');
+        $cif = $request->request->get('cif');
+        $dir = $request->request->get('direccion');
+        $prov = $request->request->get('provincia');
+
+        $empresa = $em->getRepository(Empresa::class)->findOneBy(['correo' => $buscar]);
+        $empresa->setCorreo($correo);
+        $empresa->setCif($cif);
+        $empresa->setNombre($nombre);
+        $empresa->setTelefono($tel);
+        $empresa->setDireccion($dir);
+        $empresa->setProvincia($prov);
+
+        $em->persist($empresa);
+        $em->flush();
+
+        return new JsonResponse(['modificado' => true]);
+    }
+
+    /**
+     * @Route("/modificarUsuario", name="modificar_usuario")
+     */
+    public function modificarUsuario(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $buscar = $request->request->get('aBuscar');
+        $correo = $request->request->get('correo');
+        $nombre = $request->request->get('nombre');
+        $tel = $request->request->get('telefono');
+        $dni = $request->request->get('dni');
+        $ape = $request->request->get('apellidos');
+        $rol = $request->request->get('rol');
+
+        $usu = $em->getRepository(Usuario::class)->findOneBy(['correo' => $buscar]);
+        $usu->setCorreo($correo);
+        $usu->setDni($dni);
+        $usu->setNombre($nombre);
+        $usu->setApellidos($ape);
+        $usu->setTelefono($tel);
+        $usu->setRol($rol);
+
+        $em->persist($usu);
+        $em->flush();
+
+        return new JsonResponse(['modificado' => true]);
+    }
 }
