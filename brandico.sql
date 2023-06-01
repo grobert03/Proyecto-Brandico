7b50dc49f5f09238b911e1373b17e4d9645b31be
rgainaQM <gainarobert47@gmail.com>
2023-06-01 15:07:38 +0200

    bbdd

--- brandico.sql
+++ brandico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-05-2023 a las 10:19:47
+-- Tiempo de generación: 01-06-2023 a las 15:06:43
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -20,87 +20,227 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `brandico`
 --
-CREATE DATABASE IF NOT EXISTS `brandico` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `brandico` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `brandico`;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `empresa`
+-- Estructura de tabla para la tabla `comentarios`
 --
 
-CREATE TABLE `empresa` (
-  `correo` varchar(100) NOT NULL,
-  `clave` varchar(20) NOT NULL,
-  `cif` varchar(9) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `telefono` varchar(9) NOT NULL,
-  `direccion` varchar(500) NOT NULL,
-  `provincia` varchar(100) NOT NULL
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `id_post` int(11) NOT NULL,
+  `autor` int(11) NOT NULL,
+  `fecha` date NOT NULL DEFAULT current_timestamp(),
+  `contenido` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `seguidos`
+-- Estructura de tabla para la tabla `likes`
 --
 
-CREATE TABLE `seguidos` (
-  `seguidor` varchar(100) NOT NULL,
-  `seguido` varchar(100) NOT NULL
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `id_post` int(11) NOT NULL,
+  `id_comentario` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Estructura de tabla para la tabla `publicaciones`
 --
 
-CREATE TABLE `usuario` (
-  `correo` varchar(100) NOT NULL,
-  `clave` varchar(20) NOT NULL,
-  `dni` varchar(9) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `apellidos` varchar(100) NOT NULL,
-  `telefono` varchar(9) NOT NULL,
-  `rol` tinyint(1) NOT NULL DEFAULT 0
+CREATE TABLE `publicaciones` (
+  `id` int(11) NOT NULL,
+  `autor` int(11) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
+  `texto` text DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `publicaciones`
+--
+
+INSERT INTO `publicaciones` (`id`, `autor`, `fecha`, `texto`, `imagen`) VALUES
+(7, 1, '2023-06-01 10:00:00', 'aaaaaa', 'logo-quality.png'),
+(8, 1, '2023-06-01 11:00:00', 'Buenas tardes', 'Cat_November_2010-1a.jpg'),
+(9, 2, '2023-06-01 11:30:00', 'Holaaa', 'Cat_August_2010-4.jpg'),
+(10, 3, '2023-06-01 11:31:00', 'kdlsajdaskldjskaldjsakldjasas', 'international-cat-day1-scaled.jpg'),
+(11, 1, '2023-06-01 12:31:00', 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', 'gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg'),
+(12, 2, '2023-06-01 13:00:00', 'Holaaa', 'logo-quality.png'),
+(13, 1, '2023-06-01 13:20:00', 'Holaaa', 'Flag_of_Barcelona.svg.png'),
+(14, 1, '2023-06-01 14:10:00', 'xd', 'tienda.jpg'),
+(15, 1, '2023-06-01 14:48:29', 'prueba', 'Flag_of_the_Land_of_Valencia_(official).svg.png'),
+(16, 1, '2023-06-01 14:49:27', '9', 'Bandera_de_la_ciudad_de_Madrid.svg.png'),
+(17, 1, '2023-06-01 14:50:52', '10', 'Flag_of_the_Land_of_Valencia_(official).svg.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seguidores`
+--
+
+CREATE TABLE `seguidores` (
+  `id` int(11) NOT NULL,
+  `id_seguido` int(11) NOT NULL,
+  `id_seguidor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `seguidores`
+--
+
+INSERT INTO `seguidores` (`id`, `id_seguido`, `id_seguidor`) VALUES
+(1, 1, 2),
+(2, 1, 3),
+(3, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `clave` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `foto` varchar(100) NOT NULL DEFAULT 'default.png',
+  `es_empresa` tinyint(1) NOT NULL DEFAULT 0,
+  `rol` tinyint(1) NOT NULL DEFAULT 0,
+  `telefono` varchar(20) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `provincia` varchar(255) DEFAULT NULL,
+  `recuperacion` varchar(50) DEFAULT NULL,
+  `expiracion_rec` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `correo`, `clave`, `nombre`, `foto`, `es_empresa`, `rol`, `telefono`, `direccion`, `provincia`, `recuperacion`, `expiracion_rec`) VALUES
+(1, 'prueba@mail.com', '$2y$10$SE5ZiXksq20NUBMIJvZ76uSYzyFPQ6Fz5bEdAh4ilcXjcxTA0Qg.2', 'Prueba', 'default.png', 0, 1, NULL, NULL, NULL, NULL, NULL),
+(2, 'robert@mail.com', '$2y$10$uYsyTxE8B1pXzCrkuHcqJu70uA7oH0aloS4qwspeqjRybuP0FMB9q', 'Robert', 'default.png', 0, 1, NULL, NULL, NULL, NULL, NULL),
+(3, 'willy@mail.com', '$2y$10$uYsyTxE8B1pXzCrkuHcqJu70uA7oH0aloS4qwspeqjRybuP0FMB9q', 'Willy', 'default.png', 0, 0, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `empresa`
+-- Indices de la tabla `comentarios`
 --
-ALTER TABLE `empresa`
-  ADD PRIMARY KEY (`correo`),
-  ADD UNIQUE KEY `cif` (`cif`);
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `autor` (`autor`),
+  ADD KEY `id_post` (`id_post`);
 
 --
--- Indices de la tabla `seguidos`
+-- Indices de la tabla `likes`
 --
-ALTER TABLE `seguidos`
-  ADD PRIMARY KEY (`seguidor`,`seguido`),
-  ADD KEY `se_em` (`seguido`);
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_comentario` (`id_comentario`),
+  ADD KEY `id_post` (`id_post`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
--- Indices de la tabla `usuario`
+-- Indices de la tabla `publicaciones`
 --
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`correo`),
-  ADD UNIQUE KEY `dni` (`dni`);
+ALTER TABLE `publicaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `autor` (`autor`);
+
+--
+-- Indices de la tabla `seguidores`
+--
+ALTER TABLE `seguidores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_seguido` (`id_seguido`),
+  ADD KEY `id_seguidor` (`id_seguidor`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo` (`correo`),
+  ADD UNIQUE KEY `telefono` (`telefono`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT de la tabla `seguidores`
+--
+ALTER TABLE `seguidores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `seguidos`
+-- Filtros para la tabla `comentarios`
 --
-ALTER TABLE `seguidos`
-  ADD CONSTRAINT `se_em` FOREIGN KEY (`seguido`) REFERENCES `empresa` (`correo`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `se_us` FOREIGN KEY (`seguidor`) REFERENCES `usuario` (`correo`) ON UPDATE CASCADE;
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`autor`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_post`) REFERENCES `publicaciones` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`id_comentario`) REFERENCES `comentarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`id_post`) REFERENCES `publicaciones` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `likes_ibfk_3` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  ADD CONSTRAINT `publicaciones_ibfk_1` FOREIGN KEY (`autor`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `seguidores`
+--
+ALTER TABLE `seguidores`
+  ADD CONSTRAINT `seguidores_ibfk_1` FOREIGN KEY (`id_seguido`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `seguidores_ibfk_2` FOREIGN KEY (`id_seguidor`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
