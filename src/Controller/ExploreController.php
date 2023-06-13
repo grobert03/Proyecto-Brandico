ace86432a02d3e49432c395705e1ef280c9bf3ae
grobert03 <gainarobert47@gmail.com>
2023-06-13 10:26:22 +0200

    explorar

--- src/Controller/ExploreController.php
+++ src/Controller/ExploreController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Usuario;
+use App\Entity\Comentario;
+use App\Entity\Seguidor;
+use App\Entity\Publicacion;
+use App\Entity\Like;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -45,5 +49,93 @@ class ExploreController extends AbstractController {
         }
 
         return new JsonResponse($reponse);
+    }
+
+    function devolverSeguidos($usu = 0) {
+        $em = $this->getDoctrine()->getManager();
+        if (!$usu) {
+            $usu = $this->getUser()->getId();
+        }
+        
+        $usuarios = $em->getRepository(Seguidor::class)->findBy(['id_seguidor' => $usu]);
+        $lista = [];
+
+        foreach($usuarios as $u) {
+            array_push($lista, $u->getId_seguido()->getId());
+        }
+
+        return $lista;
+
+    }
+
+    /**
+     * @Route("/devolverPublicacionesExplorar", name="devolver_publicaciones_explorar")
+     */
+    public function devolverPubExplorar(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $pagina = $request->get('pagina');
+        // Devolver usuarios a los que sigue
+        $lista = $this->devolverSeguidos();
+        $condicion = '';
+        foreach ($lista as $i => $l) {
+            $condicion = $condicion.$l;
+            if (sizeof($lista) != $i + 1) {
+                $condicion = $condicion.',';
+            } 
+        }
+        // Devolver publicaciones
+        $qb = $em->getRepository(Publicacion::class)->createQueryBuilder('p');
+        $qb->where('p.autor not in (:condicion)')->setParameter('condicion', $condicion)->andWhere('p.autor != :usu')->setParameter('usu', $this->getUser()->getId())->setMaxResults(3)->setFirstResult(($pagina - 1) * 3)->orderBy('p.fecha', 'DESC');
+
+        $publicaciones = $qb->getQuery()->getResult();
+
+        $json = [];
+        foreach ($publicaciones as $p) {
+            $likes = $em->getRepository(Like::class)->findBy(['id_post' => $p->getId()]);
+            $le_gusta = $em->getRepository(Like::class)->findOneBy(['id_post' => $p->getId(), 'id_usuario' => $this->getUser()->getId()]);
+            // Devolver comentarios
+            $comentarios = $em->getRepository(Comentario::class)->findBy(['id_post' => $p->getId()], ['fecha' => 'DESC']);
+            $array_comentarios = [];
+            $array_personas = [];
+            $personas = $em->getRepository(Like::class)->findBy(['id_post' => $p->getId()]);
+            foreach($personas as $pe) {
+                if ($pe->getId_usuario()->getId() != $this->getUser()->getId()) {
+                    array_push($array_personas, ['id' => $pe->getId_usuario()->getId(), 'nombre' => $pe->getId_usuario()->getNombre(), 'foto' =>  'img/perfiles/'.$pe->getId_usuario()->getFoto()]);
+                }
+                
+            }
+
+
+            foreach ($comentarios as $c) {
+                $likes_com = $em->getRepository(Like::class)->findBy(['id_comentario' => $c->getId()]);
+                $le_gusta_com = $em->getRepository(Like::class)->findOneBy(['id_usuario' => $this->getUser()->getId(), 'id_comentario' => $c->getId()]);
+                array_push($array_comentarios, ['id' => $c->getId(), 'correo' => $c->getAutor()->getCorreo(), 'autor' => $c->getAutor()->getNombre(), 'foto' => 'img/perfiles/'.$c->getAutor()->getFoto() , 'fecha' => $c->getFecha(), 'contenido' => $c->getContenido(), 'likes' => sizeof($likes_com), 'le_gusta' => $le_gusta_com ? 1 : 0]);
+            }
+
+            if ($le_gusta) {
+                $le_gusta = true;
+            } else {
+                $le_gusta = false;
+            }
+            $json[] = [
+                'id' => $p->getId(),
+                'autor' => $p->getAutor()->getNombre(),
+                'perfil' => 'img/perfiles/'.$p->getAutor()->getFoto(),
+                'correo' => $p->getAutor()->getCorreo(),
+                'fecha' => $p->getFecha(),
+                'le_gusta' => $le_gusta,
+                'likes' => sizeof($likes),
+                'texto' => $p->getTexto(),
+                'imagen' => $p->getImagen() ? 'img/publicaciones/'.$p->getImagen() : null,
+                'video' => $p->getVideo() ? 'videos/publicaciones/'.$p->getVideo() : null,
+                'comentarios' => $array_comentarios,
+                'personas' => $array_personas
+            ];
+        }
+        // Ordenar por likes
+        usort($json, function($a, $b) {
+            return $a['likes'] < $b['likes'];
+        });
+        return new JsonResponse($json);
     }
 }
